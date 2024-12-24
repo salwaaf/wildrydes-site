f54b074bbe3eb276d4a2e50d20a11ce9ea8f213a
salwaaf <86474103+salwaaf@users.noreply.github.com>
2024-12-24 17:01:47 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_6wPyjauPm', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '1fe9h482ouid3g2l3mc9l2qeq5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'eu-north-1' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
